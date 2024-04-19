e9266ee45db3d16b2f46e017bf595f69f734b760
SufiyanAbbasi <115076329+SufiyanAbbasi@users.noreply.github.com>
2024-04-19 17:13:45 +0500

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nzwalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Doe", "Bobs", "harry" };
+            return Ok(studentNames);
+        }
+    }
+}
